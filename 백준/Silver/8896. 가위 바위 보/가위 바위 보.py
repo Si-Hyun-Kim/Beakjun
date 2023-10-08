af3de08bb8096cf6b8e983f1602bdd9a2eaa2d20
Kim Sihyun <132899179+Si-Hyun-Kim@users.noreply.github.com>
2023-10-08 21:47:36 +0900

    [Silver IV] Title: 가위 바위 보, Time: 152 ms, Memory: 117128 KB -BaekjoonHub

--- 백준/Silver/8896. 가위 바위 보/가위 바위 보.py
+++ 백준/Silver/8896. 가위 바위 보/가위 바위 보.py
@@ -1,7 +1,6 @@
 def RSP(i,n):
-    val_list=list(i.values())
     if n>=len(i[1]):
-        char_list=[item[-1] for item in val_list]
+        char_list=[item[-1] for item in i]
         if ('R' in char_list) and ('S' in char_list) and ('P' in char_list):
             print(0)
         elif char_list.count('R')==1 or char_list.count('S')==1 or char_list.count('P')==1:
@@ -12,7 +11,7 @@ def RSP(i,n):
         else:
             print(0)
     else:
-        char_list=[item[n] for item in val_list]
+        char_list=[item[n] for item in i]
         if ('R' in char_list) and ('S' in char_list) and ('P' in char_list):
             RSP(i,n+1)
         elif (char_list.count('R')+char_list.count('X')==len(char_list)) or (char_list.count('S')+char_list.count('X')==len(char_list)) or (char_list.count('P')+char_list.count('X')==len(char_list)):
@@ -23,22 +22,22 @@ def RSP(i,n):
                     for j in char_list:
                         if j==lose:
                             p=char_list.index(j)
-                            dict_index.append(p+1)
+                            index.append(p)
                             char_list[p]=''
-                            for k in dict_index:
+                            for k in index:
                                 i[k]='X'*len(i[1])
-                            dict_index.clear()
+                            index.clear()
             RSP(i,n+1)
 T=int(input())
-robot=[]
+robot=[[] for i in range(T)]
 RPS_string=(('R','S'),('S','P'),('P','R'))
-dict_index=[]
+index=[]
 for i in range(T):
-    num={}
+    num=[]
     N=int(input())
     for j in range(N):
         string=input()
-        num[j+1]=string
-    robot.append(num)
+        num.append(string)
+    robot[i]=num
 for i in robot:
-    RSP(i,0)
+    RSP(i,0) 
